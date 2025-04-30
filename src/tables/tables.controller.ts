1f51587448074147a026fa6a547c1dc0f874362d
Maietskyi <damaietskyi@gmail.com>
2025-04-30 23:19:14 +0300

    Project

--- src/tables/tables.controller.ts
+++ src/tables/tables.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Post, Body, Controller } from '@nestjs/common';
+import { TablesService } from './tables.service';
+import { CreateTableDto } from './dto/create-table.dto';
+import { Table } from './entities/table.entity';
 
 @Controller('tables')
-export class TablesController {}
+export class TablesController {
+  constructor(private readonly tableService: TablesService) {}
+
+  @Post()
+  async create(@Body() createTableDto: CreateTableDto): Promise<Table> {
+    return await this.tableService.create(createTableDto);
+  }
+}
