a9f2391fc695830a251612b160169a6ea35338be
Maietskyi <damaietskyi@gmail.com>
2025-05-04 21:51:57 +0300

    Lesson-1

---
+++ src/auth/entities/user.entity.ts
@@ -0,0 +1,19 @@
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import bcrypt from 'bcrypt';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @BeforeInsert()
+  async hashPassword() {
+    this.password = await bcrypt;
+  }
+}
